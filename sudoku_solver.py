aabf114b1319b70edc4c5d1b855e3199e7f1c650
originalang <angeorge200@gmail.com>
2018-09-15 17:48:35 -0400

    Added notes for the basic structure, as planned during the meeting on thursday

---
+++ sudoku_solver.py
@@ -0,0 +1,10 @@
+# the cell class
+## value (initialize with value as 0)
+## enter_value()
+
+# the board class
+## init(9x9 list of lists)
+## print_board()
+## is_full()
+## find_possibilities()
+## solve()
